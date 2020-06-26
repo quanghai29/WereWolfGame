cac4979980077c065281ffc3412f9eb417917ef1
QUANG HAI <lequanghai540@gmail.com>
2020-06-26 08:35:26 +0700

    home design

--- middlewares/sockets.middlewares.js
+++ middlewares/sockets.middlewares.js
@@ -1,8 +1,16 @@
 var socketio = require('socket.io');
 module.exports = function (app, server) {
     const io =  socketio.listen(server);
+    const room ="123456";
+    const members = ["hai","ngo","chua","ngao","thuong"];
     io.on("connection",(socket)=>{
         console.log("co nguoi truy cap: "  + socket.id);
-        
+        //join to a room
+        socket.join(room);
+        io.in(room).clients((err,clis) => {
+            console.log(clis);
+        });
     })
+   
+   
 }
